9f7184ff36c35b79ae7876b333baeabcb34f1065
ÜzeyirCan <140662384+uzeyircan@users.noreply.github.com>
2024-12-13 18:04:36 +0300

    son1

--- cv/src/sections/Skills/Skills.jsx
+++ cv/src/sections/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "@/sections/Skills/SkillsStyles.module.css";
+import styles from "../Skills/SkillStyles.module";
 import checkMarkIcon from "../../assets/checkmark-dark.svg";
 import SkillList from "../../common/SkillList";
 function Skills() {
