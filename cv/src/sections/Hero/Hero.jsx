b4157caed09a34543a13124ea903e041766f35ca
ÜzeyirCan <140662384+uzeyircan@users.noreply.github.com>
2024-12-13 16:26:32 +0300

    a

--- cv/src/sections/Hero/Hero.jsx
+++ cv/src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/hero-img.png";
+import heroImg from "cv/src/assets/hero-img.webp";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
 import twitterLight from "../../assets/twitter-light.svg";
