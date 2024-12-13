f3f28fb52d20d4a47227dab64feb582c4fdee4de
ÜzeyirCan <ucanadali@gmail.com>
2024-12-13 16:01:09 +0300

    s

--- cv/src/sections/Skills/Skills.jsx
+++ cv/src/sections/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./SkillsStyles.module.css"; // Eğer aynı klasördeyse
+import styles from "@/sections/Skills/SkillsStyles.module.css";
 import checkMarkIcon from "../../assets/checkmark-dark.svg";
 import SkillList from "../../common/SkillList";
 function Skills() {
--- cv/vite.config.js
+++ cv/vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"], // Dosya uzantılarını kontrol edin
+    alias: {
+      "@": "/src", // Proje kökünü src ile eşleştiriyoruz
+    },
   },
   build: {
     rollupOptions: {
