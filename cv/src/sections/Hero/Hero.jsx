2fc30f80a87ee255a9585f707480e38fe311dba3
ÜzeyirCan <140662384+uzeyircan@users.noreply.github.com>
2026-01-02 00:03:31 +0300

    cv

--- cv/src/sections/Hero/Hero.jsx
+++ cv/src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/CV-6.pdf";
+import CV from "../../assets/CV - Üzeyircan Adalı.pdf";
 import { useTheme } from "../../common/ThemeContext";
 function Hero() {
   const { theme, toggleTheme } = useTheme();
